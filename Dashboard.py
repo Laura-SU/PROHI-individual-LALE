bea611268457cad90fda48d58450c7779c0a363c
Laura-SU <LALE7029@student.su.se>
2025-09-14 17:03:09 +0200

    updated dashboard and about page
    Updated dashboard to include all product categories and regions.
    Completed the about page text.

--- Dashboard.py
+++ Dashboard.py
@@ -13,18 +13,18 @@ st.sidebar.success("Select a tab above.")
 
 # # Page information
 
-st.write("# Welcome to PROHI Dashboard! 👋")
+st.write("# Wholesale product sales dashboard. Welcome! 👋")
 
 # INPUT WIDGETS 
-selected_region = st.selectbox("Select region", ["Lisbon", "Oporto"])
+selected_region = st.selectbox("Select region", ["Lisbon", "Oporto", "Other region"])
 selected_channel = st.radio("Select channel", ["Hotel, restaurant and cafe", "Retail"])
-selected_products = st.multiselect("Choose product categories", ["Fresh", "Milk", "Grocery"])
+selected_products = st.multiselect("Choose product categories", ["Fresh", "Frozen", "Milk", "Grocery", "Delicatessen", "Detergents"])
 
 # DATA ELEMENT
 st.markdown("Product data")
-df = pd.DataFrame(np.random.randn(10, 3), columns=["Fresh", "Milk", "Grocery"])
+df = pd.DataFrame(np.random.randint(100, 5000, size=(10, 6)), columns=["Fresh", "Frozen", "Milk", "Grocery", "Delicatessen", "Detergents"])
 st.dataframe(df)
 
 # CHART
 st.markdown("Product chart")
-st.bar_chart(df[["Fresh", "Milk", "Grocery"]].sum())
+st.bar_chart(df[["Fresh", "Frozen", "Milk", "Grocery", "Delicatessen", "Detergents"]].sum())
--- pages/About.py
+++ pages/About.py
@@ -4,10 +4,9 @@ st.title("Laura Lemetti")
 
 st.markdown(
 """
-### Project Summary  
-
-For my DSHI project, I worked with a dataset about wholesales customers and their annual spending of different product categories.  
-I applied exploratory data analysis (EDA) and created visual summaries.  
-
+For my DSHI project, I worked with the Wholesale Customers Dataset from the UCI Machine Learning Repository. 
+The dataset contains annual spending data for customers across several product categories such as Fresh, Milk, Grocery, Frozen, Detergents, and Delicatessen. 
+The project started with exploratory data analysis (EDA). After that, I used clustering techniques to group customers with similar purchasing behaviours and used classification methods to predict which customers were likely to be big spenders on the chosen product category. 
+The code created in the project can be used by suppliers to plan their inventory and create targeted marketing campaigns. In the Dashboard page the supplier can choose product categories, see spending data, and view a chart of the total spending. 
 """
 )

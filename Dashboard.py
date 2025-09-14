5c84c475b8d4c597730d8a8a78df6ffe9d6a2d5e
Laura-SU <LALE7029@student.su.se>
2025-09-14 15:54:09 +0200

    First version of dashboard and about page created

--- Dashboard.py
+++ Dashboard.py
@@ -1,7 +1,9 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.set_page_config(
-    page_title="PROHI Dashboard",
+    page_title="PROHI Dashboard LALE7029",
     page_icon="👋",
 )
 
@@ -13,47 +15,16 @@ st.sidebar.success("Select a tab above.")
 
 st.write("# Welcome to PROHI Dashboard! 👋")
 
-st.markdown(
-"""
-    ## Aims
+# INPUT WIDGETS 
+selected_region = st.selectbox("Select region", ["Lisbon", "Oporto"])
+selected_channel = st.radio("Select channel", ["Hotel, restaurant and cafe", "Retail"])
+selected_products = st.multiselect("Choose product categories", ["Fresh", "Milk", "Grocery"])
 
-    After completing the course the student should be able to:
-    - explain basic project management methods
-    - be able to account for success factors in Health Informatics projects
-    - understand basic methods and tools in the field of data science and machine learning
-    - explain process models for data mining projects
-    - explain the difference between rule-based methods and machine learning methods
-    - apply basic project management methods
-    - work in an international multidisciplinary project group
-    - independently lead and implement a limited project in health informatics - document the steps in the design of a prototype for a health informatics project
-    - apply the steps in a process model for data mining projects
-    - apply methods from the field of text mining on different types of health informatics problems
-    - explain and argue for their positions regarding the implementation of a health informatics project
-    - explain how to work with sensitive health information in a safe and ethical way.
+# DATA ELEMENT
+st.markdown("Product data")
+df = pd.DataFrame(np.random.randn(10, 3), columns=["Fresh", "Milk", "Grocery"])
+st.dataframe(df)
 
-"""
-)
-
-# You can also add text right into the web as long comments (""")
-"""
-The final project aims to apply data science concepts and skills on a 
-medical case study that you and your team select from a public data source.
-The project assumes that you bring the technical Python skills from 
-previous courses (*DSHI*: Data Science for Health Informatics), as well as 
-the analytical skills to argue how and why specific techniques could
-enhance the problem domain related to the selected dataset.
-"""
-
-### UNCOMMENT THE CODE BELOW TO SEE EXAMPLE OF INPUT WIDGETS
-
-# # DATAFRAME MANAGEMENT
-import numpy as np
-
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
-
-# # Add a slider to the sidebar:
-add_slider = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0)
- )
+# CHART
+st.markdown("Product chart")
+st.bar_chart(df[["Fresh", "Milk", "Grocery"]].sum())
---
+++ pages/About.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Laura Lemetti")
+
+st.markdown(
+"""
+### Project Summary  
+
+For my DSHI project, I worked with a dataset about wholesales customers and their annual spending of different product categories.  
+I applied exploratory data analysis (EDA) and created visual summaries.  
+
+"""
+)
